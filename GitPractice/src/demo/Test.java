9bb4eefa3bd7f09a2fd520ef6ac6ad7e9174f42b
Vrushali <mahajanvrushali15@gmail.com>
2023-06-27 10:59:28 +0530

    This is my first practice project

---
+++ GitPractice/src/demo/Test.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("do work......................");
+		}
+	}
+
+}
